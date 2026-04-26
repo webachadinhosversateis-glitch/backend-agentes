7aca8dd808e455cd1a625669b6b97d4350510e0f
webachadinhosversateis-glitch <webachadinhosversateis@gmail.com>
2026-04-26 11:08:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
@@ -8,4 +9,8 @@ def home():
 
 @app.get("/agente")
 def agente():
-  return {"resposta": "agente funcionando"}
+    return {"resposta": "agente funcionando"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
